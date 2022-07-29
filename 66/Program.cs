b408ca38e24fd434c89aadfe3c81e82adcfaf2da
lomekc <lomekc@mail.ru>
2022-07-29 09:50:06 +0300

    66

---
+++ 66/Program.cs
@@ -0,0 +1,24 @@
+﻿int SumOfNaturalElem(int I, int J)
+{
+    if (i < j)
+    {
+        while (i != j)
+            return i + SumOfNaturalElem(i + 1, j);
+    }
+
+    if (i > j)
+    {
+        while (i != j)
+            return i + SumOfNaturalElem(i - 1, j);
+    }
+
+    return i;
+}
+
+Console.Write("введите 1 число: ");
+int I = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите 2 число: ");
+int J = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("сумма натуральных элементов в указанном промежутке: " + SumOfNaturalElem(I, J));
