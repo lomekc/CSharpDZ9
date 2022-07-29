f20c7bf12135417a5e6fef9172cf25bf77a99012
lomekc <lomekc@mail.ru>
2022-07-29 09:46:40 +0300

    64

---
+++ 64/Program.cs
@@ -0,0 +1,12 @@
+﻿int NumberOfDigits(int N)
+{
+    if (N / 10 != 0)
+        return 1 + NumberOfDigits(N / 10);
+
+    else return 1;
+}
+
+Console.Write("введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("количетсво цифр в указанном числе: " + NumberOfDigits(m));
